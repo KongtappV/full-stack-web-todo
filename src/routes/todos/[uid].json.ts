aea295685917011958142373875d69efdbc54291
KongtappV <kongtapp.v@ku.th>
2022-02-19 01:02:07 +0700

    add patch endpoint - mark as done

--- src/routes/todos/[uid].json.ts
+++ src/routes/todos/[uid].json.ts
@@ -8,7 +8,7 @@ export const del = (request) => {
 export const patch = async (request) => {
   const data = await request.request.formData();
   return api(request, {
-    text: data.get("text").toString(),
+    text: data.get("text"),
     done: data.has("done") ? !!data.get("done") : undefined
   });
 }
