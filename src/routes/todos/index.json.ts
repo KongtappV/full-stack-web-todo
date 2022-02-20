a6d13d989d00e13b5a8796ca2832fea688d14d86
Kongtapp Veerawattananun <kongtapp.v@ku.th>
2022-02-20 03:30:44 +0000

    Use Prisma ORM

--- src/routes/todos/index.json.ts
+++ src/routes/todos/index.json.ts
@@ -9,7 +9,6 @@ export const get = (request) => {
 export const post = async (request) => {
   const data = await request.request.formData();
   return api(request, {
-    uid: `${Date.now()}`, // TODO: Replace with the UID from the datbase
     created_at: new Date(),
     text: data.get("text").toString(),
     done: false
